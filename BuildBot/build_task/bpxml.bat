a741b5f8359c6eae29bd5ff2df890f5ec574380f
wildfishyu <wildfishyu@gmail.com>
2016-04-27 20:10:12 +0800

    Delete bpxml.bat

--- BuildBot/build_task/bpxml.bat
+++
@@ -1,6 +0,0 @@
-SET _SLN_FILE=%_BUILDBOT_SOLUTION%
-SET _SPD_FILE=%_BUILDBOT_SVN_PATH%\_BinR\KCBPSPD_wlfs.xml
-
-echo %_SLN_FILE%
-echo %_SPD_FILE%
-python %~dp0gen_spd.py "%_SLN_FILE%" "%_SPD_FILE%"
